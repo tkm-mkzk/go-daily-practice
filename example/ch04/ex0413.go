dd66c15b11885dec97e7513295d93af8e06ab558
tkm-mkzk <tkm.mkzk.0604@gmail.com>
2025-04-06 08:57:51 +0900

    ラベル

--- example/ch04/ex0413.go
+++ example/ch04/ex0413.go
@@ -3,7 +3,7 @@ package main
 import "fmt"
 
 func ex0413() {
-	evenVals := []int{2, 4, 6, 8, 10, 12} 
+	evenVals := []int{2, 4, 6, 8, 10, 12}
 	for i, v := range evenVals {
 		fmt.Println(i, v)
 	}
--- example/ch04/main.go
+++ example/ch04/main.go
@@ -1,5 +1,5 @@
 package main
 
 func main() {
-	ex0416()
+	ex0419()
 }
